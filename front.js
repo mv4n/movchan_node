98817f29add8788cf01f1ef2268929150b3a250a
mv4n <movchanzahar777@gmail.com>
2025-02-17 16:57:30 +0200

    task_05

--- front.js
+++ front.js
@@ -1,75 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
     const messagesContainer = document.querySelector('#messages');
     const sendMessageForm = document.querySelector('#sendMessageForm');
-    const userNameInput = sendMessageForm.querySelector('#userName');
     const messageText = sendMessageForm.querySelector('#messageText');
-    let username = '';
-    let id = '';
+
     messageText.focus();
     messageText.value = '';
 
-
-
     sendMessageForm.addEventListener('submit', e => {
         e.preventDefault();
-        username = userNameInput.value.trim();
-        const text = messageText.value.trim();
-        if (text && username) {
+        const message = messageText.value.trim();
+        if (message) {
             sendMessageForm.messageText.value = '';
-            fetch(`/newMessage?username=${username}&text=${text}&id=${id ? id : null}`, {})
+            fetch(`/newMessage?message=${message}`, {})
                 .then(response => response.json())
                 .then(data => {
                     messagesContainer.innerHTML = '';
-                    id = data.id;
-                    localStorage.setItem('id', id);
-                    data.messages.forEach(message => {
-                        messagesContainer.insertAdjacentHTML('beforeend', `
-                            <div class="message-item ${message.id === id ? 'mine' : ''}">
-                                ${message.text} <sub>by ${message.username}</sub>
-                            </div>
-                        `);
+                    data.forEach(message => {
+                        messagesContainer.insertAdjacentHTML('beforeend',`
+                            <div class="message-item">${message}</div>
+                        ` );
                     })
-                })
+
+                });
         }
     });
 
-    function getMessages() {
+    setInterval(() => {
         fetch(`/getMessages`, {})
             .then(response => response.json())
             .then(data => {
+                console.log(data);
                 messagesContainer.innerHTML = '';
                 data.forEach(message => {
-                    messagesContainer.insertAdjacentHTML('beforeend', `
-                            <div class="message-item ${message.id === id ? 'mine' : ''}">
-                                ${message.text} <sub>by ${message.username}</sub>
-                            </div>
-                        `);
-                });
-                getMessages();
-            })
+                    messagesContainer.insertAdjacentHTML('beforeend',`
+                            <div class="message-item">${message}</div>
+                        ` );
+                })
 
-    }
-
-    function getMessagesLogin() {
-
-        fetch(`/getMessagesLogin`, {})
-            .then(response => response.json())
-            .then(data => {
-                messagesContainer.innerHTML = '';
-                data.forEach(message => {
-                    console.log(message)
-                    messagesContainer.insertAdjacentHTML('beforeend', `
-                            <div class="message-item ${message.id === id ? 'mine' : ''}">
-                                ${message.text} <sub>by ${message.username}</sub>
-                            </div>
-                        `);
-                });
-                getMessages();
-            })
-    }
-
-    id = localStorage.getItem('id');
-    getMessagesLogin();
+            });
+    }, 1000)
 })
 
 
--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 const path = require('path');
 
 const messages = [];
-let responses = [];
-const users = [];
 
 const server = http.createServer((req, res) => {
     const reqURL = url.parse(req.url, true);
@@ -26,43 +24,25 @@ const server = http.createServer((req, res) => {
                 })
                 res.end(data);
             })
-            break
+            break;
+
         case '/newMessage':
-            const username = searchParams.get('username');
-            let id = searchParams.get('id');
-            const text = searchParams.get('text');
-
-            if (id === 'null') {
-                id = users.length ? +users.at(-1).id + 1 : 0;
-            }
-            users.push({ id: id });
-
-            const newMessage = { username, text, id };
-            if (username && text) {
+            const newMessage = searchParams.get('message');
+            if (newMessage) {
                 messages.push(newMessage);
-
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-
-                const lastMessages = messages.slice(-20);
-                res.end(JSON.stringify({ messages: lastMessages, id }));
-
-                responses.forEach(r => {
-                    r.writeHead(200, { 'Content-Type': 'application/json' });
-                    r.end(JSON.stringify(lastMessages));
-                });
-                responses = [];
+                const messagesResponse = JSON.stringify(messages);
+                res.writeHead(200, {
+                    'Content-Type': 'application/json',
+                })
+                res.end(messagesResponse);
             }
             break;
 
         case '/getMessages':
-            responses.push(res);
-            break;
-
-        case '/getMessagesLogin':
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-
-            const lastMessagesLogin = messages.slice(-20);
-            res.end(JSON.stringify(lastMessagesLogin));
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+            });
+            res.end(JSON.stringify(messages));
             break;
 
         case '/style.css':
@@ -77,7 +57,8 @@ const server = http.createServer((req, res) => {
                 })
                 res.end(data);
             })
-            break
+            break;
+
         case '/front.js':
             fs.readFile(path.join(__dirname, 'front.js'), 'utf8', (err, data) => {
                 if (err) {
@@ -90,17 +71,18 @@ const server = http.createServer((req, res) => {
                 })
                 res.end(data);
             })
-            break
+            break;
+
         case '/favicon.ico':
             res.writeHead(404);
             res.end('no icon');
             break;
+
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end("404");
     }
 
-})
+});
 
-
-server.listen(5000, () => {})
+server.listen(5000, () => {});
